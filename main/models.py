96fef57c458b7ea0c5f0e61ca51d877e871a7217
yassine-youcefi <mohamed.youcefi.etu@univ-mosta.dz>
2021-10-17 15:29:16 +0100

    update quiz application

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=200)
+    topic = models.CharField(max_length=200)
+    number_of_questions = models.IntegerField(default=0)
+    time = models.IntegerField(help_text="Time in minutes", default=0)
+    required_score = models.IntegerField(help_text="Score required to pass the quiz")
+
+    def __str__(self):
+        return self.name
+
+    def get_questions(self):
+        return self.question_set.all()
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # answer = models.CharField(max_length=200)
+    # points = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.text
+
+    def get_answers(self):
+        return self.answer_set.all()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    cereated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post,User
+from django.views.generic import ListView
 
-# Create your views here.
+
+
+def index(request):
+    
+    return render(request, 'templates/index.html')
+
+
